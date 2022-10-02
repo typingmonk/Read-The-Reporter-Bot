b707baa9914a44c6cc6dd8b8a917c3be2a173b36
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-02 14:23:14 +0800

    make toot.sh executable.

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 
 # create essential file and folder
-
 cp $BASEDIR/config.example.py $BASEDIR/config.py
 mkdir $BASEDIR/rss_feed
+
+# make .sh file executable.
+chmod +x $BASEDIR/toot.sh
 
 # create python venv
 python3 -m venv $BASEDIR/bot_venv
