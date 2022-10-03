6ace9280b57735d7fc2c4c299c3753cc3302a9e7
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-03 11:53:01 +0800

    Make build.sh act like a bundle.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$PWD
 
 # create essential file and folder
 cp $BASEDIR/config.example.py $BASEDIR/config.py
@@ -11,3 +11,7 @@ chmod +x $BASEDIR/toot.sh
 
 # create python venv
 python3 -m venv $BASEDIR/bot_venv
+
+# substitute {BASEDIR} in cron and toot.sh
+sed -i "s/{BASEDIR}/$BASEDIR/g" $BASEDIR/toot.sh
+sed -i "s/{BASEDIR}/$BASEDIR/g" $BASEDIR/cron
